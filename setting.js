4e5612cdc44b3596028c8e8d74889ea2ab611fe7
FBOTZ <106799919+fbotz193@users.noreply.github.com>
2022-09-15 19:30:25 +0700

    Update setting.js

--- setting.js
+++ setting.js
@@ -10,17 +10,17 @@ global.Api = {
 global.ApiKey = 'coderevolution' //JIKA INGIN MENDAPATKAN KOMISI DAFTAR DI HTTPS://AMPIBISMM.MY.ID UNTUK MENDAPATKAN APIKEY!
 
 global.smm = {
-  nomor : '6283102650464',
-  atasnama : 'Ahmad Nur Fadhil'
+  nomor : '6283891032106',
+  atasnama : 'fbotz'
 }
 
 // Other
-global.owner = ['6283102650464',"6282135724604"]
-global.premium = ['6283102650464']
+global.owner = ['6283863353792',"6283891032106"]
+global.premium = ['6283863353792']
 global.sponsor = 'no sponsors in:('
-global.packname = 'Crizzy-Bot'
-global.footer = '© 2022 Fadhil Graphy'
-global.author = 'Owner : Fadhil Graphy\nIg1 : @fdhlgrphy\nIg2 : @nr.fdhl__'
+global.packname = 'VIP ID'
+global.footer = '© 2022 VIP ID'
+global.author = 'Owner : VIP ID\nIg1 : @vip_id\nIg2 : @vip_id'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.act = {
 		wait: 'please wait a moment, processing data...',
